59dcb5beecfda50957bf3f7436c746729a67f121
sooaKwon02 <102012142+sooaKwon02@users.noreply.github.com>
2022-03-21 14:35:46 +0900

    Create 2588.java

---
+++ 2588.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		int b;
+
+		a = sc.nextInt();
+    b = sc.nextInt();
+		System.out.println(a*(b%10));
+		System.out.println(a*(b%100/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+    
+    sc.close();
+
+	}
+
+}
